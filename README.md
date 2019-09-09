ab29fdd7fddb08c0224caf73e3eb3fdcfb4000e4
matsalinas <matias.salinas.s@mail.pucv.cl>
2019-09-09 18:50:03 +0000

    Readme with screenshots

---
+++ README.md
@@ -0,0 +1,12 @@
+<style>
+img[src$="center"] {
+  display:block;
+  margin: 0 auto;
+}
+</style>
+
+# EOL Conditional XBlock
+XBlock to show/hide one or more conditional component by a trigger component in the Open edX LMS. Editable within Open edx Studio.
+
+# Screenshots
+![Screenshot-example](eolconditional/examples/09-09-2019.png?style=center)
--- setup.py
+++ setup.py
@@ -26,8 +26,8 @@ def package_data(pkg, roots):
 setup(
     name='eolconditional-xblock',
     version='0.1',
-    description='eolconditional XBlock',   # TODO: write a better description.
-    license='UNKNOWN',          # TODO: choose a license: 'AGPL v3' and 'Apache 2.0' are popular.
+    description='Conditional XBlock with trigger and conditional components',
+    license='AGPL v3',
     packages=[
         'eolconditional',
     ],
