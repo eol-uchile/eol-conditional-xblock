cab12be08eddcf229afeaa28bbbe20e0ae5f6fdd
Jromo <jromo.dcc@gmail.com>
2021-04-27 15:16:09 -0400

    prueba trims

--- eolconditional/static/js/src/eolconditional.js
+++ eolconditional/static/js/src/eolconditional.js
@@ -5,10 +5,10 @@ function EolConditionalXBlock(runtime, element, settings) {
         var handlerUrl = runtime.handlerUrl(element, 'publish_completion');
         $('.vert').filter('[data-id*="' + settings.location + '"]').hide(); // Hide eolconditional Xblock
         set_visibility();
-        var submit_button = $('.submit').filter('[aria-describedby*="' + settings.trigger_component + '"]');
+        var submit_button = $('.submit').filter('[aria-describedby*="' + settings.trigger_component.trim() + '"]');
         var submit_button_text = submit_button.find('span:first').text(); // getting 'original state' of submit button
         var jsdng = false;
-        if($('.vert').filter('[data-id*="' + settings.trigger_component + '"]').find('.jsinput[data-getstate]').length > 0){
+        if($('.vert').filter('[data-id*="' + settings.trigger_component.trim() + '"]').find('.jsinput[data-getstate]').length > 0){
             jsdng = true; //is a Javascript Display and Grading problem
             submit_button.attr('no-click','true');
         }
@@ -18,21 +18,21 @@ function EolConditionalXBlock(runtime, element, settings) {
             //TODO: improve this code
             var refreshIntervalId = setInterval(function() {
                 if(jsdng){
-                    let new_no_click = $('.submit').filter('[aria-describedby*="' + settings.trigger_component + '"]').attr('no-click');
+                    let new_no_click = $('.submit').filter('[aria-describedby*="' + settings.trigger_component.trim() + '"]').attr('no-click');
                     if (new_no_click != 'true') {
                         clearInterval(refreshIntervalId);
                         set_visibility(scroll = true);
-                        submit_button = $('.submit').filter('[aria-describedby*="' + settings.trigger_component + '"]');
+                        submit_button = $('.submit').filter('[aria-describedby*="' + settings.trigger_component.trim() + '"]');
                         submit_button.attr('no-click','true');
                         submit_button.click(query_await);
                     };
                 }
                 else{
-                    let new_submit_button_text = $('.submit').filter('[aria-describedby*="' + settings.trigger_component + '"]').find('span:first').text();
+                    let new_submit_button_text = $('.submit').filter('[aria-describedby*="' + settings.trigger_component.trim() + '"]').find('span:first').text();
                     if (new_submit_button_text == submit_button_text) {
                         clearInterval(refreshIntervalId);
                         set_visibility(scroll = true);
-                        submit_button = $('.submit').filter('[aria-describedby*="' + settings.trigger_component + '"]');
+                        submit_button = $('.submit').filter('[aria-describedby*="' + settings.trigger_component.trim() + '"]');
                         submit_button.click(query_await);
                     };
                 }
@@ -40,7 +40,7 @@ function EolConditionalXBlock(runtime, element, settings) {
         }
 
         function set_visibility(scroll = false) {
-            if (is_visible(settings.trigger_component)) {
+            if (is_visible(settings.trigger_component.trim())) {
                 $.ajax({
                     type: "POST",
                     url: handlerUrl,
@@ -54,6 +54,7 @@ function EolConditionalXBlock(runtime, element, settings) {
                 );
 
                 for (const [index, conditional_component] of settings.conditional_component_list.entries()) {
+                    conditional_component = conditional_component.trim();
                     let c = $('.vert').filter('[data-id*="' + conditional_component + '"]');
                     c.show();
                     // Scroll page to the first conditional_component on submit
@@ -63,6 +64,7 @@ function EolConditionalXBlock(runtime, element, settings) {
                 }
             } else {
                 for (conditional_component of settings.conditional_component_list) {
+                    conditional_component = conditional_component.trim();
                     let c = $('.vert').filter('[data-id*="' + conditional_component + '"]');
                     c.hide();
                 }
@@ -71,16 +73,19 @@ function EolConditionalXBlock(runtime, element, settings) {
 
         // Set visible if submit button is disabled or answer is correct
         function is_visible(component) {
+            component = component.trim();
             return check_disabled(component) || check_status(component);
         }
 
         // Check if submit button is disabled (finished attempting the problem) and if the problem has been answered
         function check_disabled(component) {
+            component = component.trim();
             return $('.submit').filter('[aria-describedby*="' + component + '"]').is(":disabled") && !$('.status').filter('[id*="status_' + component + '"]').hasClass('unanswered');
         }
 
         // Check if answer is correct
         function check_status(component) {
+            component = component.trim();
             let is_correct = true;
             let elements = $('.status').filter('[id*="status_' + component + '"]');
             // Sometimes are more than one status element
