c9e52d4b72791b0ceb8ecde8ffef8bc3c3591c68
catalinaIlabaca <catalina.a.ilabaca@uchile.cl>
2025-04-21 18:21:06 -0400

    test: adds several tests to improve coverage
    - adds tests to try publish_completion fuction, studio and author render view
    
    - adds test to try possible options in publish_completion function with error in data

--- eolconditional/tests.py
+++ eolconditional/tests.py
@@ -143,3 +143,93 @@ class TestEolConditionalXBlock(UrlResetMixin, ModuleStoreTestCase):
         response = self.xblock.studio_submit(request)
         self.assertEqual(self.xblock.trigger_component, 'trigger_component')
         self.assertEqual(self.xblock.conditional_component, 'conditional_component')
+
+
+    def test_publish_completion(self):
+        """
+            Test publish completion @XBlock.json_handler
+        """
+        request = TestRequest()
+        request.method = 'POST'
+        post_data = {
+            'completion': 0.5,
+            'dispatch':'test'
+        }
+        data = json.dumps(post_data)
+        request.body = data.encode('utf-8')
+        response = self.xblock.publish_completion(request)
+        self.assertEqual(response.json_body['result'], "ok")
+    
+    def test_publish_completion_error_completion_data_range(self):
+        """
+            Test publish completion @XBlock.json_handler error in completion data range
+        """
+        request = TestRequest()
+        request.method = 'POST'
+        post_data = {
+            'completion': 2.5,
+            'dispatch':'test'
+        }
+        data = json.dumps(post_data)
+        request.body = data.encode('utf-8')
+        response = self.xblock.publish_completion(request)
+        self.assertEqual(response.json_body['error'], "Invalid completion value 2.5. Must be in range [0.0, 1.0]")
+    
+    def test_publish_completion_error_completion_data_type(self):
+        """
+            Test publish completion @XBlock.json_handler error in completion data type
+        """
+        request = TestRequest()
+        request.method = 'POST'
+        post_data = {
+            'completion': '2.5',
+            'dispatch':'test'
+        }
+        data = json.dumps(post_data)
+        request.body = data.encode('utf-8')
+        response = self.xblock.publish_completion(request)
+        self.assertEqual(response.json_body['error'], "Invalid completion value 2.5. Must be a float in range [0.0, 1.0]")
+    
+    def test_publish_completion_completion_service_is_none(self):
+        """
+            Test publish completion @XBlock.json_handler error in No completion service found
+        """
+        # Mock runtime.service to return None
+        self.xblock.runtime.service = Mock(return_value=None)
+        request = TestRequest()
+        request.method = 'POST'
+        data = json.dumps({})
+        request.body = data.encode('utf-8')
+        response = self.xblock.publish_completion(request)
+        self.assertEqual(response.json_body['error'], "No completion service found")
+    
+    def test_publish_completion_completion_service_completion_tracking_enabled_False(self):
+        """
+            Test publish completion @XBlock.json_handler error in Completion tracking is not enabled
+        """
+        # Mock runtime.service.completion_tracking_enabled to return False
+        mock_completion_service = Mock()
+        mock_completion_service.completion_tracking_enabled.return_value = False
+        self.xblock.runtime.service = Mock(return_value=mock_completion_service)
+        request = TestRequest()
+        request.method = 'POST'
+        data = json.dumps({})
+        request.body = data.encode('utf-8')
+        response = self.xblock.publish_completion(request)
+        self.assertEqual(response.json_body['error'], "Completion tracking is not enabled and API calls are unexpected")
+
+    def test_studio_view_render(self):
+        """
+            Check if xblock studio template loaded correctly
+        """
+        studio_view = self.xblock.studio_view()
+        studio_view_html = studio_view.content
+        self.assertIn('id="settings-tab"', studio_view_html)
+
+    def test_author_view_render(self):
+        """
+            Check if xblock  author template loaded correctly
+        """
+        author_view = self.xblock.author_view()
+        author_view_html = author_view.content
+        self.assertIn('class="eolconditional_block"', author_view_html)
